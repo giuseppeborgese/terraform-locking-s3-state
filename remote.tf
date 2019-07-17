ce273ebe97aa49b7bc5ff282849b42b0ae384005
Giuseppe Borgese <giuseppeborgese@gmail.com>
2019-07-17 14:56:38 +0200

    some files

---
+++ remote.tf
@@ -0,0 +1,44 @@
+/*
+terraform {
+ backend "s3" {
+ encrypt = true
+ bucket = "terraform-remote-state-storage-s3-for-youtube"
+ region = "us-east-1"
+ key = "youtube/terraform.tfstate"
+ dynamodb_table = "terraform-state-lock-dynamo"
+ }
+}
+
+
+resource "aws_s3_bucket" "terraform-state-storage-s3" {
+    bucket = "terraform-remote-state-storage-s3-for-youtube"
+
+    versioning {
+      enabled = true
+    }
+
+    lifecycle {
+      prevent_destroy = true
+    }
+
+    tags = {
+      Name = "S3 Remote Terraform State Store"
+    }
+}
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "terraform-state-lock-dynamo"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "DynamoDB Terraform State Lock Table"
+  }
+}
+*/
---
+++ security_groups.tf
@@ -0,0 +1,17 @@
+/*
+resource "aws_security_group" "sg" {
+  vpc_id      = "vpc-3901d841"
+  name        = "Testlock"
+}
+
+variable "number" { default = 10 }
+resource "aws_security_group_rule" "rule" {
+  count             =  var.number
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = count.index
+  to_port           = count.index
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg.id
+}
+*/
